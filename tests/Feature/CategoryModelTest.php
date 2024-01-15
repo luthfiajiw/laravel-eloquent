2f1df9408a5a681fbce705a9f8d8e140e775c90f
Luthfi Aji <luthfi.aji@firstasiaconsultants.tech>
2024-01-15 15:17:57 +0700

    query global scope

--- tests/Feature/CategoryModelTest.php
+++ tests/Feature/CategoryModelTest.php
@@ -3,11 +3,15 @@
 namespace Tests\Feature;
 
 use App\Models\Category;
+use App\Models\Scopes\IsActiveScope;
+use Database\Seeders\CategorySeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
+use function PHPUnit\Framework\assertEquals;
 use function PHPUnit\Framework\assertNotNull;
+use function PHPUnit\Framework\assertNull;
 
 class CategoryModelTest extends TestCase
 {
@@ -23,5 +27,36 @@ class CategoryModelTest extends TestCase
         $category->save();
 
         assertNotNull($category->id);
+    }
+
+    public function testMassUpdate()
+    {
+        $this->seed(CategorySeeder::class);
+
+        $request = [
+            "name" => "Food Updated",
+            "description" => "Food Category Updated"
+        ];
+
+        $category = Category::find('FOOD');
+        $category->fill($request);
+        $category->save();
+
+        assertEquals("Food Updated", $category->name);
+    }
+
+    public function testIsActiveScope()
+    {
+        $category = new Category();
+        $category->id = 'FOOD';
+        $category->name = 'Food';
+        $category->description = 'Food Category';
+        $category->save();
+
+        $category = Category::find('FOOD');
+        assertNull($category);
+
+        $category = Category::withoutGlobalScope(IsActiveScope::class)->find('FOOD');
+        assertNotNull($category);
     }
 }
--- tests/Feature/VoucherModelTest.php
+++ tests/Feature/VoucherModelTest.php
@@ -3,11 +3,13 @@
 namespace Tests\Feature;
 
 use App\Models\Voucher;
+use Database\Seeders\VoucherSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 use function PHPUnit\Framework\assertNotNull;
+use function PHPUnit\Framework\assertNull;
 
 class VoucherModelTest extends TestCase
 {
@@ -19,5 +21,19 @@ class VoucherModelTest extends TestCase
         $voucher->save();
 
         assertNotNull($voucher->id);
-    }  
+    }
+
+    public function testSoftDeleteVoucher()
+    {
+        $this->seed(VoucherSeeder::class);
+        
+        $voucher = Voucher::query()->where('name', 'Pulsa')->first();
+        $voucher->delete();
+
+        $voucher = Voucher::query()->where('name', 'Pulsa')->first();
+        assertNull($voucher);
+
+        $voucher = Voucher::withTrashed()->where('name', 'Pulsa')->first();
+        assertNotNull($voucher);
+    }
 }

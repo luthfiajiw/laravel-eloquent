32ad615819c4b6ae19a1a92d1f29dee2f6ce4fdb
Luthfi Aji <luthfi.aji@firstasiaconsultants.tech>
2024-01-17 15:20:39 +0700

    pivot

---
+++ app/Models/Like.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+class Like extends Pivot
+{
+    protected $table = 'customers_likes_products';
+    protected $foreignKey = 'customer_id';
+    protected $relatedKey = 'product_id';
+    public $timestamps = true;
+
+    public function customer() : BelongsTo
+    {
+        return $this->belongsTo(Customer::class, "customer_id", "id"); 
+    }
+
+    public function product() : BelongsTo
+    {
+        return $this->belongsTo(Product::class, "product_id", "id");    
+    }
+}
